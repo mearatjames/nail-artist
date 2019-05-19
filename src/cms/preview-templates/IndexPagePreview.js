94268831c67ae071121d092e8703af1577030a6a
Mearat Hou <mearatjames@gmail.com>
2019-05-18 19:57:23 -0700

    Test

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -3,20 +3,19 @@ import PropTypes from 'prop-types'
 import { IndexPageTemplate } from '../../templates/index-page'
 
 const IndexPagePreview = ({ entry }) => {
-  // const data = entry.getIn(['data']).toJS()
+  const data = entry.getIn(['data']).toJS()
 
-  // if (data) {
-  //   return (
-  //     <IndexPageTemplate title={data.title} />
-  //   )
-  // } else {
-  //   return <div>Loading...</div>
-  // }
-  return (
-    <div>
-      <h1>Test</h1>
-    </div>
-  )
+  if (data) {
+    return (
+      <IndexPageTemplate 
+      title={data.title}
+      heading={data.heading}
+      subheading={data.subheading}
+      />
+    )
+  } else {
+    return <div>Loading...</div>
+  }
 }
 
 IndexPagePreview.propTypes = {
