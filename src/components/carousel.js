618dd9f16c26fc75ea45b088da8b716cbdae55ff
Mearat Hou <mearatjames@gmail.com>
2019-05-19 13:15:57 -0700

    Hook up Instagram API to Carousel component

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -12,34 +12,6 @@ import { autoPlay } from "react-swipeable-views-utils"
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews)
 
-const tutorialSteps = [
-  {
-    label: "San Francisco – Oakland Bay Bridge, United States",
-    imgPath:
-      "https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60",
-  },
-  {
-    label: "Bird",
-    imgPath:
-      "https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60",
-  },
-  {
-    label: "Bali, Indonesia",
-    imgPath:
-      "https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80",
-  },
-  {
-    label: "NeONBRAND Digital Marketing, Las Vegas, United States",
-    imgPath:
-      "https://images.unsplash.com/photo-1518732714860-b62714ce0c59?auto=format&fit=crop&w=400&h=250&q=60",
-  },
-  {
-    label: "Goč, Serbia",
-    imgPath:
-      "https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60",
-  },
-]
-
 const styles = theme => ({
   root: {
     maxWidth: 400,
@@ -57,15 +29,26 @@ const styles = theme => ({
     display: "block",
     maxWidth: 400,
     overflow: "hidden",
-    width: "100%",
+    margin: '0 auto'
   },
 })
 
 class SwipeableTextMobileStepper extends React.Component {
   state = {
     activeStep: 0,
+    ig : []
   }
 
+  componentDidMount() {
+    fetch('https://api.instagram.com/v1/users/self/media/recent/?access_token=26412791.bbafa33.5d964afbcc134587b761f8d47059b5d7&count=10')
+        .then(response => {
+            if (!response.ok) {
+            throw new Error('HTTP error, status = ' + response.status);
+            }
+            return response.json()
+        })
+        .then(response => this.setState({ig: response.data}))
+  }
   handleNext = () => {
     this.setState(prevState => ({
       activeStep: prevState.activeStep + 1,
@@ -85,26 +68,23 @@ class SwipeableTextMobileStepper extends React.Component {
   render() {
     const { classes, theme } = this.props
     const { activeStep } = this.state
-    const maxSteps = tutorialSteps.length
+    const maxSteps = this.state.ig.length
 
     return (
-      <div className={classes.root}>
-        <Paper square elevation={0} className={classes.header}>
-          <Typography>{tutorialSteps[activeStep].label}</Typography>
-        </Paper>
+      <Paper className={classes.root}>
         <AutoPlaySwipeableViews
           axis={theme.direction === "rtl" ? "x-reverse" : "x"}
           index={activeStep}
           onChangeIndex={this.handleStepChange}
           enableMouseEvents
         >
-          {tutorialSteps.map((step, index) => (
-            <div key={step.label}>
+          {this.state.ig.map((item, index) => (
+            <div key={item.id}>
               {Math.abs(activeStep - index) <= 2 ? (
                 <img
                   className={classes.img}
-                  src={step.imgPath}
-                  alt={step.label}
+                  src={item.images.standard_resolution.url}
+                  alt={ !item.caption ? 'Instagram Feeds' : item.caption}
                 />
               ) : null}
             </div>
@@ -144,7 +124,7 @@ class SwipeableTextMobileStepper extends React.Component {
             </Button>
           }
         />
-      </div>
+      </Paper>
     )
   }
 }
