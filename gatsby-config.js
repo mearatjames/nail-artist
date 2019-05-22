a366fbf3705a2f44db31e9e6dc1a2d69a3371e2c
Mearat Hou <mearatjames@gmail.com>
2019-05-22 09:47:27 -0700

    fixing material-ui styling bug

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,18 @@ module.exports = {
     author: `Mearat`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-material-ui`,
+      // If you want to use styled components, in conjunction to Material-UI, you should:
+      // - Change the injection order
+      // - Add the plugin
+      options: {
+        // stylesProvider: {
+        //   injectFirst: true,
+        // },
+      },
+      // 'gatsby-plugin-styled-components',
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
