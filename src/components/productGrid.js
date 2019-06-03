3636952a4a4314da16cac7328f7b1f81b9b709bb
Mearat Hou <mearatjames@gmail.com>
2019-06-02 22:00:59 -0700

    layout fix

--- src/components/productGrid.js
+++ src/components/productGrid.js
@@ -80,7 +80,7 @@ const ProductGrid = props => {
   )
 
   return (
-    <Grid style={{ maxWidth: "1024px", margin: '0 auto' }} justify="center" container>
+    <Grid style={{ maxWidth: "1024px", margin: '0 auto', padding: '30px 15px' }} justify="center" container>
       {data.allShopifyProduct.edges.map((item, index) => (
         <Grid className={classes.gridItem} item xs={12} sm={4} key={index}>
           <Paper style={{ padding: 15, height: "100%" }}>
